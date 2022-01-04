04aad80523862d3d24c1b7f85d4d4583860ed135
guilherme <guilherme.hlg.eng@gmail.com>
2022-01-04 15:58:50 -0300

    modulo 2 - parte 2 - componente TransactionalTable

--- 02-dt-money/src/styles/global.ts
+++ 02-dt-money/src/styles/global.ts
@@ -33,7 +33,7 @@ export const GlobalStyle = createGlobalStyle`
         -webkit-font-smoothing: antialiased;
     }
     
-    body, input, textarea, button {
+    body, input, textarea, button  {
         font-family: 'Poppins', sans-serif;
         font-weight: 400;
     }
