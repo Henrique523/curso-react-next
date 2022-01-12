ede48f4d158a1ca0e51c75cea31fc22a5ce1c94f
guilherme <guilherme.hlg.eng@gmail.com>
2022-01-12 13:22:01 -0300

    modulo 2 - parte 4

--- 02-dt-money/src/App.tsx
+++ 02-dt-money/src/App.tsx
@@ -1,12 +1,31 @@
 import { GlobalStyle } from "./styles/global";
-import { Header } from "./components/Header/Header";
+import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import Modal from "react-modal";
+import {useCallback, useState} from "react";
+import {NewTransactionModal} from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    const handleOpenNewTransactionModal = useCallback(() => {
+        setIsNewTransactionModalOpen(true);
+    }, [isNewTransactionModalOpen]);
+
+    const handleCloseNewTransactionModal = useCallback(() => {
+        setIsNewTransactionModalOpen(false);
+    }, [isNewTransactionModalOpen]);
+
+
   return (
     <>
         <GlobalStyle />
-        <Header />
+        <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
+        <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
+
         <Dashboard />
     </>
   );
