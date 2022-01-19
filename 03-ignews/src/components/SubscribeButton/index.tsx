03500dd02358fb240ed0524aca513ff55fa77522
guilherme <guilherme.hlg.eng@gmail.com>
2022-01-19 17:12:24 -0300

    modulo 3 - parte 2 - sub 3 finalizado

--- 03-ignews/src/components/SubscribeButton/index.tsx
+++ 03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,12 +1,40 @@
 import styles from './styles.module.scss';
+import {useCallback} from "react";
+import {signIn, useSession} from "next-auth/react";
+import {api} from "../../services/api";
+import {getStripeJs} from "../../services/stripeJs";
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
+// getServerSideProps (SSR) - ciclo de vida
+// getStaticProps (SSG) - ciclo de vida
+// API routes
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const { data: session } = useSession();
+
+    const handleSubscribe = useCallback(async () => {
+        if (!session) {
+            signIn('github');
+            return;
+        }
+
+        try {
+            const response = await api.post('/subscribe');
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs();
+            await stripe.redirectToCheckout({ sessionId });
+        } catch (e) {
+            console.log(e);
+            alert(e.message);
+        }
+    }, [session]);
+
     return (
-        <button type="button" className={styles.subscribeButton}>
+        <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
             Subscribe now
         </button>
     );
